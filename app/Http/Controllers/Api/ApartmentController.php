abf01a97a0ebd27c7cb354d8ea447d6bb372925a
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2023-03-10 17:29:30 +0100

    fix search api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -12,32 +12,45 @@ class ApartmentController extends Controller
 {
 
     //funzione che permette di visualizzare gli ultimi appartamenti con sponsorizzazioni ancora attive
-    public function sponsorApartment(){
-        
+    public function sponsorApartment()
+    {
+
         $apartmentslist = Apartment::with('sponsors')->get();
-        $apartmentFilteredList=[];
-       
-        foreach($apartmentslist as $apartment){
-            if(($apartment->toArray()["sponsors"]!==[])){
-                $apartmentFilteredList[]=$apartment;
+        $apartmentFilteredList = [];
+
+        foreach ($apartmentslist as $apartment) {
+            if (($apartment->toArray()["sponsors"] !== [])) {
+                $apartmentFilteredList[] = $apartment;
             }
         }
-        
-        return response()->json($apartmentFilteredList);
 
+        return response()->json($apartmentFilteredList);
     }
 
-    public function researchApartment(Request $request){
-        $data=$request->all();
-        $apartments = Apartment::query();
-        if(!is_null($request->title)){
-            $apartments->orWhere("title", "LIKE", "%{$request->title}%");
-        }
-        if(!is_null($request->num_beds)){
-            $apartments->orWhere("num_beds", ">=", "{$request->num_beds}");
-        }
-        $apartments= $apartments->get();
-        return response()->json($apartments);
+    public function researchApartment(Request $request)
+    {
+        $data = $request->all();
 
+        $apartments = Apartment::query();
+        if (!is_null($request->title)) {
+            $apartments->where("title", "LIKE", "%{$request->title}%");
+        }
+        if (!is_null($request->num_beds)) {
+            $apartments->where("num_beds", ">=", "{$request->num_beds}");
+        }
+        if (!is_null($request->num_rooms)) {
+            $apartments->where("num_rooms", ">=", "{$request->num_rooms}");
+        }
+        if (!is_null($request->num_bathrooms)) {
+            $apartments->where("num_bathrooms", ">=", "{$request->num_bathrooms}");
+        }
+        if (!is_null($request->square_meters)) {
+            $apartments->where("square_meters", ">=", "{$request->square_meters}");
+        }
+        if (!is_null($request->square_meters)) {
+            $apartments->where("square_meters", ">=", "{$request->square_meters}");
+        }
+        $apartments = $apartments->get();
+        return response()->json($apartments);
     }
 }
